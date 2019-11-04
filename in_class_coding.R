e65720271b2461d93a0f03bd6c4a3cc91d81e5e1
Tim Dennis <dennis.tim@gmail.com>
2019-11-04 12:20:31 -0800

    adding r script for class

---
+++ in_class_coding.R
@@ -0,0 +1,6 @@
+# use this document to follow along with the instructor
+# typing code and running it 
+# make a comment (use the # at the start of the line) with your full name
+# Your Name 
+# be sure to save your file periodically 
+
